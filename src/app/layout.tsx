6eafe9ca93882790247cd9468329c05111df7bfe
alan damian Fernandez <92124271+Alan-Fernandez@users.noreply.github.com>
2024-03-31 16:58:09 -0300

    Speed Insights Metrics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,15 @@
 import { Roboto } from 'next/font/google'
 import { Header } from 'app/components/shared/Header'
 import { Footer } from 'app/components/shared/Footer'
+import { injectSpeedInsights } from '@vercel/speed-insights/sveltekit';
 import 'app/sass/globals.sass'
 
 const roboto = Roboto({
   weight: ["100", "300", "500", "700"],
   subsets: ["latin"],
 })
+
+injectSpeedInsights();
 
 export default function RootLayout({
   children,
